a067b66ed767a8e0e27458cec3be9c10358b1697
Grant Eadie <grantleadie@gmail.com>
2020-08-28 08:46:45 -0700

    create id's for individual order pieces.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,38 @@
+// Business Logic for Order Form
+
 function Order() {
   this.sides = [];
   this.pizzas = [];
   this.currentId = 0;
 }
 
-function Sides(salad, drink, desert) {
-  this.salad = salad;
-  this.drink = drink;
-  this.desert = desert;
+Order.prototype.addSide = function(side) {
+  side.id = this.assignId();
+  this.sides.push(side);
 }
 
-function Pizza(cheese, toppings) {
-  this.cheese = cheese;
-  this.toppings = [];
+Order.prototype.addPizza = function(pizza) {
+  pizza.id = this.assignId();
+  this.pizzas.push(pizza);
 }
 
 Order.prototype.assignId = function() {
   this.currentId +=1;
   return this.currentId;
 }
+
+// Business Logic for Sides 
+
+function Sides(salad, drink, desert) {
+  this.salad = salad;
+  this.drink = drink;
+  this.desert = desert;
+  this.currentId = 0
+}
+
+function Pizza(cheese, toppings) {
+  this.cheese = cheese;
+  this.toppings = [];
+  this.currentId = 0
+}
+
